f32cf9d6dc901b12f6a65d38a61142f9b0aa2346
JHRunning <jeonghunn1@gmail.com>
2014-11-15 00:16:58 +0900

    modapplication deleted

--- wear/src/main/java/com/tarks/transport/ListenerService.java
+++ wear/src/main/java/com/tarks/transport/ListenerService.java
@@ -1,9 +1,17 @@
 package com.tarks.transport;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.widget.Toast;
 
 import com.google.android.gms.wearable.MessageEvent;
 import com.google.android.gms.wearable.WearableListenerService;
+
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v4.app.NotificationCompat.WearableExtender;
 
 public class ListenerService extends WearableListenerService {
 
@@ -14,6 +22,35 @@ public class ListenerService extends WearableListenerService {
 
     private void showToast(String message) {
         //Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+
+    }
+
+
+    //Temp function
+    private void noti(){
+
+        int notificationId = 001;
+// Build intent for notification content
+        Intent viewIntent = new Intent(this, StationList.class);
+        //  viewIntent.putExtra("1234", eventId);
+        PendingIntent viewPendingIntent =
+                PendingIntent.getActivity(this, 0, viewIntent, 0);
+
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_launcher)
+                        .setContentTitle("천안역")
+                        .setContentText("다음 역 : 갈산역")
+                        .setLargeIcon(BitmapFactory.decodeResource(
+                                getResources(), R.drawable.bus_background))
+                        .setContentIntent(viewPendingIntent);
+
+// Get an instance of the NotificationManager service
+        NotificationManagerCompat notificationManager =
+                NotificationManagerCompat.from(this);
+
+// Build the notification and issues it with notification manager.
+        notificationManager.notify(notificationId, notificationBuilder.build());
     }
 
 }
--- wear/src/main/java/com/tarks/transport/core/global.java
+++ wear/src/main/java/com/tarks/transport/core/global.java
@@ -1,20 +1,28 @@
 package com.tarks.transport.core;
 
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 import android.widget.Toast;
+
+import com.tarks.transport.R;
+import com.tarks.transport.StationList;
 
 /**
  * Created by JHRunning on 11/14/14.
  */
 public final class global {
 
-    static ModApplication mod = ModApplication.getInstance();
     static boolean debug_mode = true;
 
     public static void toast(String str, boolean length) {
         // Log.i("ACCESS", "I can access to toast");
-        Toast.makeText(mod, str,
-                (length ? Toast.LENGTH_LONG : Toast.LENGTH_SHORT)).show();
+//        Toast.makeText(mod, str,
+//                (length ? Toast.LENGTH_LONG : Toast.LENGTH_SHORT)).show
     }
 
 
@@ -26,6 +34,7 @@ public final class global {
     public static void log(String str){
         if(debug_mode) Log.i("Log", str);
     }
+
 
 
 }
