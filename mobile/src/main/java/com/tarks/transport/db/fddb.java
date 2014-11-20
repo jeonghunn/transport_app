7c1a12d7378df89e81bc4afc96515a83258be342
JHRunning <jeonghunn1@gmail.com>
2014-11-20 13:43:52 +0900

    fddb column added

--- mobile/src/main/java/com/tarks/transport/db/fddb.java
+++ mobile/src/main/java/com/tarks/transport/db/fddb.java
@@ -10,6 +10,7 @@ public class fddb {
 
     public static final class CreateDB implements BaseColumns {
         public static final String _TABLENAME = "flow";
+        public static final String COUNT_SRL = "count_srl";
         public static final String COUNTRY_SRL = "country_srl";
         public static final String ROUTE_SRL = "route_srl";
         public static final String STATION_SRL = "station_srl";
@@ -20,6 +21,7 @@ public class fddb {
         public static final String _CREATE =
                 "create table "+_TABLENAME+"("
                         +_ID+" integer primary key autoincrement, "
+                        +COUNT_SRL+" int not null , "
                         +COUNTRY_SRL+" int not null , "
                         +ROUTE_SRL+" int not null , "
                         +STATION_SRL+" int not null , "
