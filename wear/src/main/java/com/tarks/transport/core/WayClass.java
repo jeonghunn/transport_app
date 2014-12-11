2029eceda4df5535a5644edafbefb290ca7d3028
JHRunning <jeonghunn1@gmail.com>
2014-12-11 14:09:19 +0900

    WayClass in wear added

---
+++ wear/src/main/java/com/tarks/transport/core/WayClass.java
@@ -0,0 +1,21 @@
+package com.tarks.transport.core;
+
+/**
+ * Created by JHRunning on 11/16/14.
+ */
+
+public class WayClass {
+    public int way_srl;
+    public String station_name;
+
+
+    public WayClass(){}
+
+    public WayClass(int _way_srl, String _station_name){
+
+        this.way_srl = _way_srl;
+        this.station_name = _station_name;
+
+    }
+
+}
